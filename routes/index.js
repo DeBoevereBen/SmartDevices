7bb42358d23c3855a278ea7c347cb276e62d92bf
Kevin Baes <kevin.baes@student.howest.be>
2018-04-25 12:51:20 +0200

    Add mysqluserservice, example in index.js

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,23 @@
 var express = require('express');
 var router = express.Router();
+const MysqlUserService = require('../persistence/mysqlUserService');
 
+var userService = new MysqlUserService("arduino_racer", "kevin", "kevin"); //TODO get credentials from config fiile
 /* GET home page. */
 router.get('/', function(req, res, next) {
+    userService
+        .findUser("joske","vermeule")
+        .then(result => {
+          if(result.notfound){
+            console.log("User not found");
+          } else {
+              console.log(user);
+          }
+
+        });
+
   res.render('index', { title: 'Express' });
+
 });
 
 module.exports = router;
